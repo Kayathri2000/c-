19a7fc5b3c0d78c89ee5f2e52d7f7e541a68596a
Kayathri2000 <69625465+Kayathri2000@users.noreply.github.com>
2020-09-12 15:43:32 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,12 @@
+//to find the product of two numbers 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,product;
+    cout<<"enter the value od a and b "<<"\n";
+    cin>>a>>b;
+    product=a*b;
+    cout<<"product of "<<a<<" and "<<b<<" is "<<product;
+    return 0;
+}
