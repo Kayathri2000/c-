162f8adf53b1be7fc554356975ced76957325cee
Kayathri2000 <69625465+Kayathri2000@users.noreply.github.com>
2020-09-12 15:46:12 +0530

    Add files via upload

---
+++ size ss.cpp
@@ -0,0 +1,16 @@
+//to size of char int double and float 
+#include<iostream>
+using namespace std;
+int main()
+{
+    int integer;
+    char character;
+    float real;
+    double digits;
+    cout<<"size of int is"<<sizeof(integer)<<"\n";
+   cout<<"size of char is "<<sizeof(character)<<"\n";
+    cout<<"size of float is"<<sizeof(real)<<"\n";
+    cout<<"size of double is "<<sizeof(digits);
+    return 0;
+}
+
